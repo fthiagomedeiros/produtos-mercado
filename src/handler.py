1b5f54f809737b6b74e5a842416bad7c906cf398
Francisco Thiago de Macedo Medeiros <fthiagomedeiros@gmail.com>
2021-02-20 18:50:02 -0300

    setting up project for development

--- src/handler.py
+++ src/handler.py
@@ -1,43 +1,15 @@
 import json
 
+from src.service.get_categories import get_categories
+
 
 def hello(event, context):
-    body = {
-        "categories": [
-            {
-                "category_id": 1000,
-                "category_name": "laticinios",
-                "category_items": [
-                    {
-                        "id": 1001,
-                        "name": "leite"
-                    },
-                    {
-                        "id": 1002,
-                        "name": "queijo"
-                    }
-                ]
-            },
-            {
-                "category_id": 2000,
-                "category_name": "padaria",
-                "category_items": [
-                    {
-                        "id": 2001,
-                        "name": "pao"
-                    },
-                    {
-                        "id": 2002,
-                        "name": "biscoito"
-                    }
-                ]
-            }
-        ]
-    }
+    body = get_categories()
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body),
+        "event": json.dumps(event)
     }
 
     return response
