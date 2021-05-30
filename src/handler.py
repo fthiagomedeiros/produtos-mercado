8552649fc6b7b4c118d15702189f4206f1d5b0aa
Francisco Thiago <fthiagomedeiros@gmail.com>
2021-05-30 01:45:32 +0100

    Setting up serverless framework to create a new shopping list

--- src/handler.py
+++ src/handler.py
@@ -4,14 +4,21 @@ from src.service.service import create_new_list
 
 
 def create_list(event, context):
-    owner_id = json.loads(event['body'])['owner-id']
+
+    owner_id = json.loads(event['body'])['owner-email']
     owner_name = json.loads(event['body'])['owner-name']
-    body = create_new_list(owner_id, owner_name)
+    to_buy = json.loads(event['body'])['to-buy']
+    not_to_buy = json.loads(event['body'])['not-to-buy']
+
+    body = create_new_list(context.aws_request_id,
+                           owner_id,
+                           owner_name,
+                           to_buy,
+                           not_to_buy)
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body),
-        "event": json.dumps(event)
+        "body": json.dumps(body)
     }
 
     return response
--- src/service/service.py
+++ src/service/service.py
@@ -1,52 +1,27 @@
-categories = {
-    "id": 100,
-    "owner_name": "Francisco Medeiros",
-    "owner_email": "fthiagomedeiros@gmail.com",
-    "shared_name": "-",
-    "shared_email": "-",
-    "items_enabled": [
-        {
-            "category": "higiene",
-            "description": "Pasta de Dentes"
-        }
-    ],
-    "items_disabled": [
-        {
-            "category": "carnes",
-            "description": "Fraldinha"
-        }
-    ]
-}
-
 shopping_lists = {}
 
 
-def create_new_list(owner_id, owner_name):
-    shopping_lists['id'] = owner_id
-    shopping_lists['name'] = owner_name
+def save_list(owner_email, shopping_list):
+    if owner_email in shopping_lists.keys():
+        shopping_lists[owner_email].append(shopping_list)
+    else:
+        shopping_lists[owner_email] = []
+        shopping_lists[owner_email].append(shopping_list)
+
+
+def create_new_list(id, owner_email, owner_name, items_to_buy=None, items_not_to_buy=None):
+
+    if items_not_to_buy is None:
+        items_not_to_buy = []
+    if items_to_buy is None:
+        items_to_buy = {}
+
+    shopping_list = {'list-id': id,
+                     'owner-email': owner_email,
+                     'owner-name': owner_name,
+                     'to-buy': items_to_buy,
+                     'not-to-buy': items_not_to_buy}
+
+    save_list(owner_email, shopping_list)
+
     return shopping_lists
-
-
-def get_categories():
-    """
-    get list of categories
-    :return: the categories list
-    """
-    return categories
-
-
-def create_category(category_name, category_items):
-    """
-    Creating a new category and items
-    :param category_name: the category name
-    :param category_items: the items in the category in following format below
-                {
-                    "id": 2001,
-                    "name": "pao"
-                }
-    :return: a newly created category
-    """
-
-    categories[category_name] = category_items
-
-    return categories
